170b5366e5ef0244b1972b86efc6073b20d8d7ac
Olzhas <ozhoraye@gmu.edu>
2025-12-27 01:42:44 -0500

    Use shared paths in Figure 4 script

--- Code/11.Training-Impact.py
+++ Code/11.Training-Impact.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# Load Temp and Figures directory paths defined in paths.py
+from paths import TEMP_DIR, FIG_DIR
+
 # Read data
-file_path = '/Users/Admin/OneDrive - George Mason University - O365 Production/Documents/GMU/Dr Kugler/Skills/Reproducibility Package/Temp/Pooled_Estimates_Training.xlsx'
+#file_path = '/Users/Admin/OneDrive - George Mason University - O365 Production/Documents/GMU/Dr Kugler/Skills/Reproducibility Package/Temp/Pooled_Estimates_Training.xlsx'
+file_path = TEMP_DIR / "Pooled_Estimates_Training.xlsx"
 
 df = pd.read_excel(file_path)
 
@@ -82,7 +86,8 @@ plt.tight_layout()
 plt.subplots_adjust(left=0.25, bottom=0.15, top=0.9, right=0.95)
 
 # Save the plot
-output_file = '/Users/Admin/OneDrive - George Mason University - O365 Production/Documents/GMU/Dr Kugler/Skills/Reproducibility Package/Figures/Figure4.TrainingImpact.png'
+#output_file = '/Users/Admin/OneDrive - George Mason University - O365 Production/Documents/GMU/Dr Kugler/Skills/Reproducibility Package/Figures/Figure4.TrainingImpact.png'
+output_file = FIG_DIR / "Figure4.TrainingImpact.png"
 plt.savefig(output_file, dpi=300, bbox_inches='tight')  # Change dpi (dots per inch) for the resolution of the image
 
 # Show the plot
