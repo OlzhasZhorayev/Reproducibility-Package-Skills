8eaebe548c163fac05a056178139fb1ad467526b
Olzhas <ozhoraye@gmu.edu>
2025-12-26 20:45:06 -0500

    Create paths.py

---
+++ Reproducibility Package/Code/paths.py
@@ -0,0 +1,13 @@
+
+# Code/paths.py
+from pathlib import Path
+import os
+
+CODE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(os.environ.get("REPRO_DIR", CODE_DIR.parent))
+
+DATA_DIR = BASE_DIR / "Data"
+TEMP_DIR = BASE_DIR / "Temp"
+FIG_DIR  = BASE_DIR / "Figures"
+TABLE_DIR = BASE_DIR / "Tables"
+CODE_DIR = BASE_DIR / "Code"
