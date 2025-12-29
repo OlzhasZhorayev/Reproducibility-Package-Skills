1d9c96ec54333d0fbcc9f9e5d831b4a0161bde3c
Olzhas <ozhoraye@gmu.edu>
2025-12-29 04:01:05 -0500

    Update 00-Master-run-all.py

--- Code/00-Master-run-all.py
+++ Code/00-Master-run-all.py
@@ -37,7 +37,13 @@ if __name__ == "__main__":
     run_stata("1.Pool-Estimates-RE-All.do")
     run_stata("2.Pool-Estimates-Training.do")
     run_stata("3.Pool-Estimates-RE-Main.do")
+    run_stata("4.Pool-Estimates-FE-All.do") 
     run_stata("5.Pool-Estimates-RE-Big5.do")
+
+    # --- STATA TABLES ---
+    run_stata("6.Descriptive-Stat-Income.do")
+    run_stata("10.Descriptive-Stat-Training.do")
+    run_stata("14.Descriptive-Stat-Region.do")
 
     # --- PYTHON FIGURES ---
     run_py("7.Wage-Returns-RE.py")
@@ -45,6 +51,7 @@ if __name__ == "__main__":
     run_py("11.Training-Impact.py")
     run_py("12.Training-Heterogeneity.py")
     run_py("15.Wage-Returns-RE-Main.py")
+    run_py("16.Wage-Returns-FE-All.py")
     run_py("17.Wage-Returns-RE-Big5.py")
 
     # --- STATA FIGURES ---
